246bad20ee6719521bd853cbe2ba31eb57dc2ede
terremoth <dutra.astro@gmail.com>
2024-10-14 08:29:50 -0300

    Add more info to the users page

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,10 +18,12 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('pronouns')->nullable();
+            $table->string('phone')->nullable();
             $table->string('picture')->nullable();
             $table->text('bio')->nullable();
             $table->boolean('is_admin')->default(false);
-            $table->boolean('show_email')->default(false);
+            $table->boolean('show_email_as_image')->default(false);
+            $table->boolean('show_phone_as_image')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
